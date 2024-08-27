a7cd0d1fa23cc61fa4a59c3bf3be0d57832fb453
java-udm <huisaj30@gmail.com>
2024-08-27 17:07:07 -0500

    new configurations under java 22

--- src/test/java/com/nttdata/CucumberTestSuite.java
+++ src/test/java/com/nttdata/CucumberTestSuite.java
@@ -1,16 +1,14 @@
 package com.nttdata;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = {"pretty"},
-        features = "classpath:features",
-        tags = "@pruebaWorld"   //  ==> Definir el @tag  a ejecutar
+        features = "src/test/resources/features",
+        glue = "com.nttdata.glue",
+        plugin = {"pretty"}
 )
-public class CucumberTestSuite {
-
-
-}
+public class CucumberTestSuite {}
